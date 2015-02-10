6a46c0aeac8f2191ce400628d0e06d4606ac559b
Gabe <gsnydermail@gmail.com>
2015-02-09 19:19:17 -0500

    adding static pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   
   
   
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
   # You can have the root of your site routed with "root"
  root 'application#hello'
   
