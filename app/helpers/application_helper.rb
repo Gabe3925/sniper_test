5151ee688e8f67458baf5ddbff3f5fa157bc302d
Gabe <gsnydermail@gmail.com>
2015-02-11 23:09:57 -0500

    sniper branding

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "Ruby on Rails Tutorial Sample App"
+    base_title = "Sniper - the e-bay auction bidder"
     if page_title.empty?
       base_title
     else
