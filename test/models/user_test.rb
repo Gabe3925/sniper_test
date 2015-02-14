854e0241b9ac349a533a9cbd80fbc41188b06328
Gabe <gsnydermail@gmail.com>
2015-02-14 16:54:07 -0500

    decided to validate all emails with @ simply

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -32,16 +32,15 @@ class UserTest < ActiveSupport::TestCase
 
   test "email validation should accept valid addresses" do
     valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
-                         first.last@foo.jp alice+bob@baz.cn]
+                         first.last@foo.jp alice+bob@baz.cn mac.mad@judo.name]
     valid_addresses.each do |valid_address|
       @user.email = valid_address
       assert @user.valid?, "#{valid_address.inspect} should be valid"
     end
   end
 
-  test "email validation should reject invalid addresses" do
-    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
-                           foo@bar_baz.com foo@bar+baz.com]
+  test "email validation should reject email addresses without an '@' sign" do
+    invalid_addresses = %w[user9example,com user_at_foo.org]
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
