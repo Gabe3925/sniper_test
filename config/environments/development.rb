1e528a0a199fbf5686a1b78c340053c764c0e0cb
Gabe <gsnydermail@gmail.com>
2015-02-22 17:59:47 -0500

    mail configuration and yml file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,8 +44,8 @@ Rails.application.configure do
   address:              'smtp.gmail.com',
   port:                 587,
   domain:               'blahblah.com',
-  user_name:            'Sniper.welcome',
-  password:             '==========================put password here=============================',
+  user_name:            ENV["MAIL_USERNAME"],
+  password:             ENV["MAIL_PASSWORD"],
   authentication:       'plain',
   enable_starttls_auto: true  }
 end
