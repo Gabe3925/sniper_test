9884d0bc73af99c7cc5f0fcc0c9a341f7453ca81
Gabe <gsnydermail@gmail.com>
2015-02-27 19:28:27 -0500

    persistent login

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,9 +22,20 @@ class User < ActiveRecord::Base
     SecureRandom.urlsafe_base64
   end
 
+  #remembers the user, for persistent login
   def remember
-    self.remember_token = ...
-    update_attribute(:remember_digest, ...)
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forgets a user.
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 
 end
