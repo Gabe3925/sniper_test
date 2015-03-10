0dc43eba67ebf615e01844fe166850b53e8cb215
Gabe <gsnydermail@gmail.com>
2015-03-09 23:17:00 -0400

    pagination on users index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#how to create one seed user
+User.create!(name:  "Gabe",
+             email: "Fake@ford.foo",
+             password:              "gabeiscool",
+             password_confirmation: "gabeiscool")
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "useremail#{n+1}@fake.fazz"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
